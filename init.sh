a242ca22003c2715069d426edc5d423cfc33f8c7
Pakhapoom <pakhapoom.sar@gmail.com>
2023-07-16 15:08:17 +0700

    feat: integrate with `pre-commit`

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+current_dir=`pwd`
 cd $1
 python3 -m poetry new $2
 
 cd $2
-# current_dir=`pwd`
+python3 -m poetry add pre-commit
+cat "$current_dir/hook_configs.txt" >> pyproject.toml
+cp "$current_dir/.pre-commit-config.yaml" .pre-commit-config.yaml
+
 git init
 
 dirs=('data' 'notebooks' 'report')
