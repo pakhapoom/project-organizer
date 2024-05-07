a87f9188682bd98f1755114ce3c7397029b5bae6
Pakhapoom Sarapat <pakhapoom.sar@gmail.com>
2024-05-07 14:58:29 +0700

    remove prefix `python3 -m`

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 current_dir=`pwd`
 cd $1
-python3 -m poetry new $2
+poetry new $2
 
 cd $2
-python3 -m poetry add pre-commit
+poetry add pre-commit
 cat "$current_dir/hook_configs.txt" >> pyproject.toml
 cp "$current_dir/.pre-commit-config.yaml" .pre-commit-config.yaml
 cp "$current_dir/gitignore_starter.txt" .gitignore
